75e98e817903236658f06ab49676bbff4851c983
kevreco <kevin.rc@hotmail.fr>
2024-09-17 11:23:21 +0200

    fix tests (windows): only look for cb.c files under the ./tests/ folder

--- tests.bat
+++ tests.bat
@@ -5,7 +5,7 @@ set "root_dir=%~dp0"
 set "cb_bat=%~dp0cb.bat"
 
 @REM Recursively iterate over all the "cb.c" files and call "cb.bat" on them
-for /R "./" %%f in (cb.c) do (
+for /R "./tests/" %%f in (cb.c) do (
     if exist "%%f" (
 		@REM Go to the cb.c directory
 		cd %%~dpf

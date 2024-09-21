1f4e2d0cba16d01302fe27b7a233f5cf36ceef50
kevreco <kevin.rc@hotmail.fr>
2024-09-21 13:43:07 +0200

    fix tests build order on Github Actions

--- tests.sh
+++ tests.sh
@@ -1,17 +1,13 @@
-# OIFS and IFS are used to process path with space
-OIFS="$IFS"
-IFS=$'\n'
-
 # Get absolute path of the cb.sh script
 cb_sh=$(realpath cb.sh)
 # Get direcory of the cb.sh script
 root_dir=$(dirname $cb_sh)
 
-# Get absolute paths of the various cb.c files.
-for f in $(find $(realpath ./tests/) -iname "cb.c" ) ;
-do 
+shopt -s globstar
+for f in ./tests/**/cb.c; do
   echo "Starting test for: $f" 
-  file=$(printf "%s" "$f")
+  # Get absolute path
+  file=$(realpath "$f")
   # Get the parent directory path.
   # We strip every char from the end to the last slash
   # NOTE: dirname does not work very well with path containing spaces.
@@ -20,12 +16,11 @@ do
   cd "$dir"
   # Execute cb.sh on the current cb.c, apprently we need to use the $((XXX)) to assigne a variable in a for loop.
   $cb_sh --cxflags -std=c89 -pedantic -Werror || { exit_code=$((1)); break; }
+  # Restore original directory to get the correct absolute path
+  cd $root_dir
 done
-
-IFS="$OIFS"
 
 if [ -v exit_code ]; then
   exit $exit_code
 fi
-
 
